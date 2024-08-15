afb479888d1c624951cbc00a1020c101813b164e
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-15 12:21:34 -0300

    Create color constants

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,9 +1,11 @@
-const bgColor: string = '#1a1b1c';
-const primaryColor: string = '#ed6464';
-const secundaryColor: string = '#bf6370';
+const bgColor: string = '#111625';
+const bgItem: string = '#192036';
+const primaryColor: string = '#9d2053';
+const secundaryColor: string = '#7a1e48';
 
 export {
     bgColor,
     primaryColor,
-    secundaryColor
+    secundaryColor,
+    bgItem
 }
