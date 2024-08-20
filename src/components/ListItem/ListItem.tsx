eb60af13d7d2fe78c38300bc60b14383d27e13f4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-19 21:43:47 -0300

    Add types on my component

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,17 +1,23 @@
 import { ItemContainer, TextDescription, TextTitle, TextContainer } from "./Items.styles";
-import { FaTrash } from "react-icons/fa";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight, FaTrash } from "react-icons/fa";
 import { Item } from "../../@types/Items";
 
 const ListItems = (Props: Item) =>{
     return(
-        <ItemContainer>
+        <ItemContainer key={Props.id}>
             <TextContainer>
                 <TextTitle>{Props.title}</TextTitle>
                 <TextDescription>{Props.description}</TextDescription>
             </TextContainer>
             <div className="iconsCheck">
-                <input type="checkbox"/>
-                <FaTrash className="trashIcon" onClick={Props.onClick}/>
+                <div>
+                    <input type="checkbox"/>
+                    <FaTrash className="trashIcon" onClick={Props.onClick}/>
+                </div>
+                <div>    
+                    <FaArrowAltCircleLeft onClick={Props.toLeft} className="toLeft"/>
+                    <FaArrowAltCircleRight onClick={Props.toRight} className="toRight"/>
+                </div>
             </div>
         </ItemContainer>
     )
