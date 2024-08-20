5a424804a942385bd106ee57b0ca7e81fa28dbff
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-19 21:44:53 -0300

    Create function to move right or left card

--- src/App.tsx
+++ src/App.tsx
@@ -45,13 +45,33 @@ const App = () => {
   }
 
   function deleteItem(id: number): void{
-
-    const updateItemsList: ItemsList[] = items.filter((item: ItemsList, index: number)=>{
+    const updateItemsList: ItemsList[] = items.filter((_item: ItemsList, index: number)=>{
       return index !== id
     });
-
     setItems(updateItemsList);
+  }
 
+  function toLeftCard(id: number): void{
+    if(id > 0){
+      setItems((prevItems)=>{
+        const newItems = [...prevItems];
+        const itemToMove = newItems.splice(id, 1)[0];
+        newItems.splice(id - 1, 0, itemToMove);
+        return newItems;
+      });
+    }
+
+  }
+
+  function toRightCard(id: number): void{
+    if(id < items.length - 1){
+      setItems((allItems)=>{
+        const copyItems = [...allItems];
+        const itemToMove = copyItems.splice(id, 1)[0];
+        copyItems.splice(id + 1, 0, itemToMove);
+        return copyItems;
+      });
+    }
   }
 
   return (
@@ -76,7 +96,14 @@ const App = () => {
         <Items>
           {createItemLoading? <TextLoading>Creating Items...</TextLoading> : items.map((it, index) =>{
             return(
-                <ListItems key={index} title={it.title} description={it.description} onClick={() => deleteItem(index)}/>
+                <ListItems 
+                  id={index}
+                  title={it.title}
+                  description={it.description}
+                  onClick={() => deleteItem(index)}
+                  toLeft={()=> toLeftCard(index)}  
+                  toRight={()=> toRightCard(index)}  
+                />
             )
           })}
         </Items>
